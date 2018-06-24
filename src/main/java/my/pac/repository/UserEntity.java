2866d60253a34a5ea6fe18621c2a74b8e82d85ab
yangjwk <12345678>
2018-06-24 14:15:03 +0800

    user实体类

--- src/main/java/my/pac/repository/UserEntity.java
+++ src/main/java/my/pac/repository/UserEntity.java
@@ -1,4 +1,33 @@
 package my.pac.repository;
 
 public class UserEntity {
+    private String id;
+    private String usercode;
+    private String username;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsercode() {
+        return usercode;
+    }
+
+    public void setUsercode(String usercode) {
+        this.usercode = usercode;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
 }
